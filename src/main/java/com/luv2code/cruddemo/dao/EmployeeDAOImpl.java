a2379fb77f094748dbd3cb1d1fa62ad5481798c8
ltn.lenghia2@gmail.comgit config --global user.email ltn.lenghia2@gmail.com <138308068+ltnghia23@users.noreply.github.com>
2025-05-21 20:02:28 +0700

    update

--- src/main/java/com/luv2code/cruddemo/dao/EmployeeDAOImpl.java
+++ src/main/java/com/luv2code/cruddemo/dao/EmployeeDAOImpl.java
@@ -2,6 +2,7 @@ package com.luv2code.cruddemo.dao;
 
 import com.luv2code.cruddemo.entity.Employee;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
